a2bd7953e098f0deafe9a0536e36411f1ce580a9
Miller1999 <millerjavierarias@gmail.com>
2023-08-01 22:24:55 -0500

    Componente Navbar

--- src/pages/App/index.jsx
+++ src/pages/App/index.jsx
@@ -1,34 +1,35 @@
 //!importar lo de react
-import { useRoutes,BrowserRouter } from "react-router-dom"
+import { useRoutes, BrowserRouter } from "react-router-dom";
 //!importar componentes
-import Home from "../Home"
-import MyAccount from "../MyAccount"
-import MyOrder from "../MyOrder"
-import NotFound from "../NotFound"
-import MyOrders from "../MyOrders"
-import SignIn from "../SignIn"
+import Home from "../Home";
+import MyAccount from "../MyAccount";
+import MyOrder from "../MyOrder";
+import NotFound from "../NotFound";
+import MyOrders from "../MyOrders";
+import SignIn from "../SignIn";
+import Navbar from "../../Components/Navbar";
 //!importar css
-import './App.css'
+import "./App.css";
 
-function AppRoutes () {
+function AppRoutes() {
   let routes = useRoutes([
-    {path: "/",element:<Home />},
-    {path: "/my-account",element: <MyAccount/>},
-    {path: "/my-order",element: <MyOrder/>},
-    {path: "/my-orders",element: <MyOrders/>},
-    {path: "/sign-in",element: <SignIn/>},
-    {path: "/*",element: <NotFound/>}
-])
-  return routes
+    { path: "/", element: <Home /> },
+    { path: "/my-account", element: <MyAccount /> },
+    { path: "/my-order", element: <MyOrder /> },
+    { path: "/my-orders", element: <MyOrders /> },
+    { path: "/sign-in", element: <SignIn /> },
+    { path: "/*", element: <NotFound /> },
+  ]);
+  return routes;
 }
 
 function App() {
-  
   return (
     <BrowserRouter>
-      <AppRoutes/>
+      <AppRoutes />
+      <Navbar />
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
